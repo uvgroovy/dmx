a107fe75cf85280a300a401c9f45f3cf713819a7
Yuval Kohavi <yuval.kohavi@gmail.com>
2015-08-25 22:28:36 -0400

    load animation from file

--- dmxd/dmxd.go
+++ dmxd/dmxd.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"os"
+	"encoding/hex"
+	"encoding/json"
+	"io"
 	"fmt"
 	"github.com/uvgroovy/go-libusb"
 	"github.com/uvgroovy/dmx"
@@ -39,64 +43,31 @@ func main() {
 	// TOdo check for errors
 
 	dmxControllers := k8062.GetDmxControlers()
-
 	fmt.Printf("Got %d devices\n", len(dmxControllers))
-
 	if len(dmxControllers) == 0 {
 		return
 	}
-	dmxController := dmxControllers[0]
-
-	lightFixtures := make([]dmx.LightFixture, 0)
 	for _, d := range dmxControllers {
 		defer d.Close()
-		/*
-			for {
-				var err error = errors.New("blah")
-				var channel int
-				for err != nil {
-					fmt.Printf("Channel:")
-					_, err = fmt.Scanf("%d", &channel)
-
-				}
-
-				err = errors.New("blah")
-				var value int
-				for err != nil {
-					fmt.Printf("Value:")
-					_, err = fmt.Scanf("%d", &value)
-
-				}
-
-				d.Channels[channel] = uint8(value)
-				d.SendChannels()
-
-			}*/
-		/*
-			rand.Seed(time.Now().Unix())
-			for i :=0 ;i<512;i++{
-				var val uint8 = uint8(rand.Intn(256))
-				fmt.Printf("Seeing channel %d to %d\n",i,val)
-				d.Channels[i] = val
-				d.SendChannels()
-				time.Sleep(time.Second)
-			}*/
-		//	lightFixtures = append(lightFixtures, XL85{1})
-		// TODO: read config of config file.
-		/*
-		 RGBLightFixture{1}
-		 RGBLightFixture{4}
-		...
-		
-		*/
-		lightFixtures = append(lightFixtures, dmx.RGBLightFixture{1},dmx.RGBLightFixture{1+3*7})
 	}
+	
+	dmxController := dmxControllers[0]
 
-	animate2(dmxController, lightFixtures)
+	lightFixtures := openFixtures()
+	
+	animate2(getKeyFrames(), dmxController, lightFixtures)
+	
+	
 }
 
 func openFixtures() []dmx.LightFixture {
-	return nil
+
+	lightFixtures := make([]dmx.LightFixture, 0)
+	
+	for i := 0; i < 12; i++ {
+		lightFixtures = append(lightFixtures, 	dmx.RGBLightFixture{1 + i*3})
+	}
+	return lightFixtures
 }
 
 func animate1(dmxController dmx.DMXController, devices []dmx.LightFixture) {
@@ -112,6 +83,17 @@ func animate1(dmxController dmx.DMXController, devices []dmx.LightFixture) {
 
 }
 
+func white(dmxController dmx.DMXController, devices []dmx.LightFixture) {
+	var d dmx.DMXUniverse
+	// do whateverzz
+	sendColors(dmxController, &d, devices, dmx.Color{Red: 0xff})
+	sendColors(dmxController, &d, devices, dmx.Color{Red: 0xff})
+	
+	time.Sleep(time.Second*2)
+	sendColors(dmxController, &d, devices, dmx.Color{Red: 0xff, Green: 0xff, Blue: 0xff})
+
+}
+
 func interpolate(c1, c2 dmx.Color, r float32) dmx.Color {
 	return dmx.Color{
 		uint8(float32(c1.Red)*(1-r) + r*float32(c2.Red)),
@@ -120,31 +102,105 @@ func interpolate(c1, c2 dmx.Color, r float32) dmx.Color {
 	}
 }
 
-func animate2(dmxController dmx.DMXController, devices []dmx.LightFixture) {
-	var d dmx.DMXUniverse
-	var keyframes = []struct {
-		color dmx.Color
-		dur   time.Duration
-	}{
-		{dmx.Color{Red: 0xff}, 2 * time.Second},
-		{dmx.Color{Blue: 0xff}, 2 * time.Second},
-		{dmx.Color{Green: 0xff}, 2 * time.Second},
-		{dmx.Color{Green: 0xff, Blue: 0xff}, 2 * time.Second},
-		{dmx.Color{Red: 0xff, Green: 0xff, Blue: 0xff}, 2 * time.Second},
+type Keyframe struct {
+		Color dmx.Color
+		Duration   time.Duration
+}
+
+func parseColor(color string) dmx.Color {
+	var data [1]byte
+	hex.Decode(data[:], []byte(color[:2]))
+	r := data[0]
+	hex.Decode(data[:], []byte(color[2:4]))
+	g := data[0]
+	hex.Decode(data[:], []byte(color[4:5]))
+	b := data[0]
+	return dmx.Color{r,g,b};
+}
+
+func ReadKeyframes(reader io.Reader) []Keyframe {
+	keyframes := make([]Keyframe,0)
+	type KeyframeJson struct {
+		Color string
+		Duration  int
+	}
+	// jsonStream="[{Color:'ff00ff',Duration : 1000}]"
+	// strings.NewReader(jsonStream)
+	dec := json.NewDecoder(reader)
+	_, err := dec.Token()
+	if err != nil {
+		panic(err)
 	}
 
-	var frame time.Duration = time.Second / 60
+	var frame KeyframeJson
+	// while the array contains values
+	for dec.More() {
 
-	for index := 0; ; index = (index + 1) % len(keyframes) {
-		keyframe1 := keyframes[index]
-		keyframe2 := keyframes[(index+1)%len(keyframes)]
+		// decode an array value (Message)
+		err := dec.Decode(&frame)
+		if err != nil {
+			panic(err)
+		}
+		curDuration := int64(time.Millisecond) * int64(frame.Duration)
+		curKeyFrame := Keyframe{Color: parseColor(frame.Color), Duration: time.Duration(curDuration)}
+		keyframes = append(keyframes, curKeyFrame)
+		fmt.Printf("%v\n", curKeyFrame)
+	}
 
-		animTime := keyframe1.dur
+	// read closing bracket
+	_, err = dec.Token()
+	if err != nil {
+		panic(err)
+	}
+	return keyframes
+}
+
+var DefaultAnimation = []Keyframe{
+	{dmx.Color{Red: 0xff},  time.Second},
+	{dmx.Color{Blue: 0xff}, 2 * time.Second},
+	{dmx.Color{Green: 0xff}, 2 * time.Second},
+	{dmx.Color{Green: 0xff, Blue: 0xff}, 2 * time.Second},
+	{dmx.Color{Red: 0xff, Green: 0xff, Blue: 0xff}, 2 * time.Second},
+}
+
+func getKeyFrames() []Keyframe {
+	var input *os.File
+	
+	if len(os.Args) == 2 {
+		if os.Args[1] == "-" {
+			input = os.Stdin	
+			fmt.Println("Reading animation from stdin")
+		} else {
+			input,err := os.Open(os.Args[1])
+			if err != nil {
+				panic("can't open animation file")
+			}
+			defer input.Close()	
+			fmt.Println("Reading animation from file", os.Args[1])
+		}
+		
+		return ReadKeyframes(input)
+	} else {
+		fmt.Println("Using default animation")
+		return DefaultAnimation
+	}
+}
+
+func animate2(animation []Keyframe, dmxController dmx.DMXController, devices []dmx.LightFixture) {
+	var d dmx.DMXUniverse
+
+	var frame time.Duration = time.Second / 30
+
+	for index := 0; ; index = (index + 1) % len(animation) {
+		keyframe1 := animation[index]
+		keyframe2 := animation[(index+1)%len(animation)]
+
+		animTime := keyframe1.Duration
 		ticks := int(animTime / frame)
 
 		for i := 0; i < ticks; i++ {
 			var r float32 = float32(i) / float32(ticks)
-			newColor := interpolate(keyframe1.color, keyframe2.color, r)
+			newColor := interpolate(keyframe1.Color, keyframe2.Color, r)
 			sendColors(dmxController, &d, devices, newColor)
 			time.Sleep(frame)
 		}
